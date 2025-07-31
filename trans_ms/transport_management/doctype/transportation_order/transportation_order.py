be36d515af719c25ce58edf76491579351576aec
George Mukundi <mukundichabari@gmail.com>
2025-07-31 10:37:44 +0300

    assigned

--- trans_ms/transport_management/doctype/transportation_order/transportation_order.py
+++ trans_ms/transport_management/doctype/transportation_order/transportation_order.py
@@ -336,9 +336,8 @@ def create_sales_invoice(doc, rows):
     for row in rows:
         description = ""
         if row["assigned_vehicle"]:
-            has_assigned_trailer = any("assigned_trailer" in row)
 
-            description += "<b>" + row["assigned_vehicle"] + "/"+row["assigned_trailer"] if has_assigned_trailer else ""+"<b>"
+            description += "<b>" + row["assigned_vehicle"] + "/"+row["assigned_trailer"] if row.get("assigned_trailer") else ""+"<b>"
         # if row["route"]:
         #     description += "<BR>ROUTE: " + row["route"]
         item = frappe._dict({
